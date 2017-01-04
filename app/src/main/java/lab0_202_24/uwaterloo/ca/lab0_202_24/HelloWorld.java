e154c265a21f3d1552aee915c75a9b75c0c8f45f
katchpole <alexthorburn@hotmail.com>
2017-01-04 08:52:42 -0500

    init

---
+++ app/src/main/java/lab0_202_24/uwaterloo/ca/lab0_202_24/HelloWorld.java
@@ -0,0 +1,13 @@
+package lab0_202_24.uwaterloo.ca.lab0_202_24;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloWorld extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_world);
+    }
+}
